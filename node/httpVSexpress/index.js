fd032de37bfdb594764ccc5c0e164b805cd3b48d
Rodolfo Martínez <rodolfo@agavelab.com>
2018-07-03 19:09:52 -0500

    Implemented httpServer and middlewares

---
+++ node/httpVSexpress/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end("Hello Node.js Server!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, err => {
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+
+  console.log(`Server running at http://localhost:${port}`);
+});
