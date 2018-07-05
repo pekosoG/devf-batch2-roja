5ee1ec6a2fd92d209699f62d289b9b8675577005
Israel Garcia <igarcia@itexico.net>
2018-07-04 22:07:11 -0500

    Mongo full implementation (get, post, put & delete), firebase auth en HTML

--- node/mongo-api/index.js
+++ node/mongo-api/index.js
@@ -1,12 +1,15 @@
 var express=require('express');
 var bookRouter=require('./routes/bookRouter');
+var bodyParser=require('body-parser');
 
 var mongoose=require('mongoose');
-const db = mongoose.connect('mongodb://admin:a12345@ds127771.mlab.com:27771/mongo-api');
+const db = mongoose.connect('mongodb://admnin:a12345@ds127771.mlab.com:27771/mongo-api');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use('/book',bookRouter);
+
 
 app.listen(3000,()=>{
     console.log(`Servidor en http://localhost:3000`);
