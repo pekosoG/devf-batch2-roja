f18f5d7d1dc2f35310719818c10b1cf14bd371e9
Rodolfo Martínez <rodolfo@agavelab.com>
2018-07-06 22:00:21 -0500

    Auto training

--- chatbot/app.js
+++ chatbot/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const Bot = require('messenger-bot');
+const apiai = require('apiai');
 const app = express();
+
+const projectId = "newagent-755dd";
+const sessionId = "messenger";
+const languageCode = "es-ES";
+
+const nlp = apiai("b9d23822ccf04b58b23e852316e51710");
 
 const bot = new Bot({
   token: "XXXXX",
-  verify: "verifica-phrase",
+  verify: "verifica-devf",
   app_secret: "XXXXX",
 });
 
@@ -13,9 +20,22 @@ bot.on('error', err => {
 })
 
 bot.on('message', (payload, reply) => {
-  reply({ text: 'Holi a devf' }, err => {
-    if (err) throw err;
-  })
+  const request = nlp.textRequest(payload.message.text, {
+    sessionId: sessionId,
+  });
+
+  request.on('response', function (response) {
+    console.log(response);
+    reply({ text: response.result.fulfillment.speech }, err => {
+      if (err) throw err;
+    });
+  });
+
+  request.on('error', function (error) {
+    console.log(error);
+  });
+
+  request.end();
 })
 
 app.use(bot.middleware());
